07760a925ce194a12d76664b358d5bfcaac6464b
abhiUmap-tech <abhiumap31@gmail.com>
2025-02-11 09:58:12 +0530

    Initialize Spring Boot project for Ticket Management App

---
+++ src/main/java/com/example/ticket_managment_app/TicketManagmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticket_managment_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ticket_managment_app/TicketManagmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticket_managment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketManagmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
