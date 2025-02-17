f9e70b118ac61e5e6551243d04d986562eae82b3
abhiUmap-tech <abhiumap31@gmail.com>
2025-02-17 17:45:53 +0530

    add summary and description annotations for all endpoints to enhance Swagger UI documentation

--- src/main/java/com/example/ticket_managment_app/restcontroller/TicketController.java
+++ src/main/java/com/example/ticket_managment_app/restcontroller/TicketController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.ticket_managment_app.dto.TicketDto;
 import com.example.ticket_managment_app.service.ITicketManagementService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -27,36 +28,49 @@ public class TicketController {
     private final ITicketManagementService service;
 
     @PostMapping("/register")
+    @Operation(
+            summary = "Register a new ticket",
+            description = "Creates a new ticket based on the details provided")
     public ResponseEntity<TicketDto> registerTicket(@RequestBody TicketDto ticketDto) {
         var responseBody = service.registerTicket(ticketDto);
         return new ResponseEntity<>(responseBody, HttpStatus.CREATED);
     }
 
     @GetMapping("/findTicketById/{ticketId}")
+    @Operation(summary = "Get Ticket by ID",  
+                          description = "Fetches a ticket details based on a Unique ID")
     public ResponseEntity<TicketDto> findTicketById(@PathVariable String ticketId) {
         var responseBody = service.findTicketById(ticketId);
         return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }
 
     @GetMapping("/findAllTickets")
+    @Operation(summary = "Find all Tickets",
+                            description = "Fetches all the ticket in the database")
     public ResponseEntity<List<TicketDto>> findAllTickets() {
         var responseBody = service.findAllTickets();
         return new ResponseEntity<>(responseBody, HttpStatus.FOUND);
     }
 
     @PutMapping("/updateTicket")
+    @Operation(summary = "Updates the ticket details",  
+                            description = "Updates the ticket details based on the data provided")
     public ResponseEntity<TicketDto> updateTicket(@RequestBody TicketDto ticketDto) {
         var responseBody = service.updateTicket(ticketDto);
         return new ResponseEntity<>(responseBody, HttpStatus.CREATED);
     }
 
     @PatchMapping("/updateTicketStatus/{ticketId}/{status}")
+    @Operation(summary = "Update's ticket Status", 
+                            description = "Updates the ticket's status")
     public ResponseEntity<String> updateTicketStatus(@PathVariable String ticketId, @PathVariable String status) {
         var responseBody = service.updateTicketStatus(ticketId, status);
         return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }
 
     @DeleteMapping("/deleteTicketById/{ticketId}")
+    @Operation(summary = "Delete the ticket by ID", 
+                        description = "Delete's the ticket based on a Unique ID")
     public ResponseEntity<String> deleteTicketById(@PathVariable String ticketId) {
         var responseMessage = service.deleteTicketById(ticketId);
         return new ResponseEntity<>(responseMessage, HttpStatus.GONE);
