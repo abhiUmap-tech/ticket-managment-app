5a049a760a538dfc1f0ec9a0fd1efb2e63332cc7
abhiUmap-tech <abhiumap31@gmail.com>
2025-02-17 08:57:00 +0530

    feat: Add GlobalExceptionHandler and ErrorDetails for centralized exception handling

---
+++ src/main/java/com/example/ticket_managment_app/advices/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.ticket_managment_app.advices;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.example.ticket_managment_app.error.ErrorDetails;
+import com.example.ticket_managment_app.exceptions.InvalidStatusException;
+import com.example.ticket_managment_app.exceptions.TicketNotFoundException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TicketNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleTicketNotFoundException(TicketNotFoundException ticektNotFoundException){
+
+        var errorDetails = new ErrorDetails(ticektNotFoundException.getMessage(), 404, LocalDateTime.now());
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(InvalidStatusException.class)
+    public ResponseEntity<ErrorDetails> handleInvalidStatusException(InvalidStatusException invalidStatusException){
+        
+        var errorDetails = new ErrorDetails(invalidStatusException.getMessage(), 400, LocalDateTime.now());
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+}
